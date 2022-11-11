af403c14643b1e5f0abb08ce05fe56f31c7588bd
J.Aguasviva <Jimy.waner11@gmail.com>
2022-11-11 15:14:30 -0400

    All Errors code message and its converter added and used

--- EnadlaInventory.UI/ViewModels/SignUpVM.cs
+++ EnadlaInventory.UI/ViewModels/SignUpVM.cs
@@ -1,11 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using EnadlaInventory.Core.Communication.Models;
+using EnadlaInventory.Core.Validation.Validators;
 
 namespace EnadlaInventory.UI.ViewModels
 {
     internal class SignUpVM : ObservableObject
     {
-        private UserInfo _currentUserInfo = new UserInfo();
+        private UserInfo _currentUserInfo = new UserInfo(UserInfoValidator.RS_AUTH, UserInfoValidator.RS_SIGNUP_EXTRAS);
 
         public UserInfo CurrentUserInfo
         {
