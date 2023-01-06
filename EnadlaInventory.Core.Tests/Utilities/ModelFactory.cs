8d28dcc352103a7b4e40a38bda6d4b23f7fba9a1
J.Aguasviva <Jimy.waner11@gmail.com>
2023-01-06 13:08:06 -0400

    Adding test Utilities

---
+++ EnadlaInventory.Core.Tests/Utilities/ModelFactory.cs
@@ -0,0 +1,25 @@
+﻿using EnadlaInventory.Core.Communication.Models;
+
+namespace EnadlaInventory.Core.Tests.Utilities
+{
+    internal static class ModelFactory
+    {
+        static internal UserInfo CreateValidUserInfo()
+        {
+            return new UserInfo()
+            {
+                UID = "someId",
+                Email = "jimy.waner11@gmail.com",
+                Password = "123456789",
+                ConfirmPassword = "123456789",
+                OwnerName = "Jimy",
+                ExtraInfo = new ExtraUserInfo()
+                {
+                    UID = "someId",
+                    CreationDate = DateTime.Now,
+                    CreatorMachine = "someIdMachine"
+                }
+            };
+        }
+    }
+}
