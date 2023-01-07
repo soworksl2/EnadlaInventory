f5aa99f43cadb817a277000cc2cf635f610dff7a
J.Aguasviva <Jimy.waner11@gmail.com>
2023-01-07 13:08:39 -0400

    Cleaning the IRequestMessageSender File

--- EnadlaInventory.Core/Communication/IRequestMessageSender.cs
+++ EnadlaInventory.Core/Communication/IRequestMessageSender.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EnadlaInventory.Core.Communication
+﻿namespace EnadlaInventory.Core.Communication
 {
     public interface IRequestMessageSender
     {
