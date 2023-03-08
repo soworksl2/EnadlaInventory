9f0eaf14a606b75dda0997fa6ae8dcc48ba5fefc
J.Aguasviva <Jimy.waner11@gmail.com>
2023-03-08 15:02:29 -0400

    DomainJsonSerializerExtension static class has been added to support an easier way to deserialize or serialize jsons with a domain option

---
+++ EnadlaInventory.Core/Serialization/DomainJsonSerializerExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace EnadlaInventory.Core.Serialization
+{
+    internal static class DomainJsonSerializerExtension
+    {
+        internal static T DeserializeWithDomainOptions<T>(this JsonNode jsonNode)
+        {
+            T? output = jsonNode.Deserialize<T>();
+
+            if (output is null)
+            {
+                throw new Exception("Something went wrong deserializing");
+            }
+
+            return output;
+        }
+    }
+}
