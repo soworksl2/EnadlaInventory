f187a99743453570f6c45ac12900bc755259c8b6
J.Aguasviva <Jimy.waner11@gmail.com>
2023-03-10 12:49:51 -0400

    Add application/octet-stream to MediaTypes

--- HttpUtilities/MediaTypes.cs
+++ HttpUtilities/MediaTypes.cs
@@ -3,6 +3,7 @@
     public static class MediaTypes
     {
         public static readonly string Application_Json = "application/json";
+        public static readonly string Application_OctetStream = "application/octet-stream";
         public static readonly string Multipart_Mixed = "multipart/mixed";
     }
 }
